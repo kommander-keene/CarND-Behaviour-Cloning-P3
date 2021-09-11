6457d217524183edcbcd647a5ef3484ad06260a1
kommander-keene <keeneboondich@gmail.com>
2021-09-10 21:21:46 -0700

    commit

--- drive.py
+++ drive.py
@@ -3,6 +3,7 @@ import base64
 from datetime import datetime
 import os
 import shutil
+from tqdm import tqdm
 
 import numpy as np
 import socketio
@@ -44,7 +45,7 @@ class SimplePIController:
 
 
 controller = SimplePIController(0.1, 0.002)
-set_speed = 9
+set_speed = 30.0
 controller.set_desired(set_speed)
 
 
